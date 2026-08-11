6cc97c9548b175ba660dd51ea032e92a1daff3a6
xiuqij <lostld97@gmail.com>
2026-08-11 16:23:35 +0200

    locomotion (working, saving only pickle files)

--- locomotion.py
+++ locomotion.py
@@ -7,8 +7,7 @@ from multiprocessing import cpu_count, Pool, set_start_method, current_process
 import utils_locomotion_and_motionless as utils
 
 #%%
-folder_path_parquet =r""
-nest_path=r""   # path to pickle file
+# Constants
 mice =['red','blue','green','yellow']
 body_part = ['1_earL','2_earR','3_nose','4_centre','5_centreL','6_centreR','7_tailBase','8_tailTip']
 coord=['x','y','prob']
@@ -16,52 +15,63 @@ torso=[['4_centre_x','5_centreL_x','6_centreR_x'],['4_centre_y','5_centreL_y','6
 px_mm=0.59
 fps = 25
 px_m=px_mm*1000
-output_folder=r""
 
-parquet_files = sorted([f for f in os.listdir(folder_path_parquet) if f.endswith('.parquet')])
-with open(nest_path, 'rb') as f:
-    nest_dict = pickle.load(f)
+# Paths
+exp = 'female_P42'
+parquet_path =r"L:\Lopez Laboratory - NEURO\Xiuqi\ELA_MDMA\April_2026\parquet"
+parquet_path_exp = os.path.join(parquet_path,exp)
+nest_path=r"L:\Lopez Laboratory - NEURO\Xiuqi\ELA_MDMA\April_2026\nest"  
+nest_exp = os.path.join(nest_path,exp)
+output_path=r"L:\Lopez Laboratory - NEURO\Xiuqi\ELA_MDMA\April_2026\locomotion"
+output_path_exp = os.path.join(output_path,exp)
+time_points = ['baseline','MDMA']
 
 #%%
 def main(): #multiprocessing
     #configure_logging()
-    main_folder  = folder_path_parquet
 
-    # Gather all files to process
-    files_to_process = []
-    for file_path in glob.glob(f"{main_folder}/*.parquet"):  
+    for t in time_points:
+        print(t)
+        main_folder  = os.path.join(parquet_path_exp,t)
+        nest_file = os.path.join(nest_exp,t,'nest.pickle')
+        with open(nest_file, 'rb') as f:
+            nest_dict = pickle.load(f)
 
-        file_basename=os.path.basename(file_path)
-        file=os.path.splitext(file_basename)[0]
-        
-        files_to_process.append((file_path,main_folder,file,nest_dict,mice,body_part,coord,torso,px_mm,px_m,fps))
+        # Gather all files to process
+        files_to_process = []
+        for file_path in glob.glob(f"{main_folder}/*.parquet"):  
+            file_basename=os.path.basename(file_path)
+            file=os.path.splitext(file_basename)[0]
+            
+            files_to_process.append((file_path,main_folder,file,nest_dict,mice,body_part,coord,torso,px_mm,px_m,fps))
+        print(len(files_to_process))
+        # multiprocessing
+        num_processes=cpu_count()//3 #num of cpu cores
+        with Pool(processes=num_processes) as pool:
+            results = pool.map(utils.processer, files_to_process)
 
-    num_processes=cpu_count()//2 #num of cpu cores
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(utils.processer, files_to_process)
+        print("saving results...")
+        composite_dfs = {}
+        #composite_df=pd.DataFrame()
 
+        # Loop over the list just once
+        for item in results:
+            file=item[2]
+            entropy=item[1]
+            df=item[0]
 
-    composite_dfs = {}
-    composite_df=pd.DataFrame()
-    
+            composite_dfs[file]={'locomotion':df,'entropy':entropy}
 
-    # Loop over the list just once
-    for item in results:
-        file=item[2]
-        entropy=item[1]
-        df=item[0]
+        os.makedirs(os.path.join(output_path_exp,t),exist_ok=True)
+        file_dict=os.path.join(output_path_exp,t,'locomotion'+'.pickle')
+        with open(file_dict, "wb") as file:
+            pickle.dump(composite_dfs, file)
+            file.close()
 
-        composite_dfs[file]={'locomotion':df,'entropy':entropy}
-
-    file_dict=os.path.join(output_folder,'locomotion'+'.pickle')
-    with open(file_dict, "wb") as file:
-        pickle.dump(composite_dfs, file)
-        file.close()
-
-    #for item in results:
-      #  dataframe=item[0]
-       # composite_df = pd.concat([composite_df,dataframe], ignore_index=True)
-        #composite_df.to_csv(os.path.join(output_folder,f'locomotion.csv'),index=False)
+        #for item in results:
+        #  dataframe=item[0]
+        # composite_df = pd.concat([composite_df,dataframe], ignore_index=True)
+            #composite_df.to_csv(os.path.join(output_folder,f'locomotion.csv'),index=False)
 
 if __name__ == '__main__':
     set_start_method('spawn',force=True)  # Set the start method to 'spawn'
