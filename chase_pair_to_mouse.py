d93cc5467f2bdfcecc37ddae259bddbebc2fb4d4
xiuqij <lostld97@gmail.com>
2026-08-24 08:56:20 +0200

    compute mouse level events and customized functions for regrouping chases events in timebins

--- chase_pair_to_mouse.py
+++ chase_pair_to_mouse.py
@@ -2,13 +2,11 @@
 import os
 import pandas as pd
 
-
-#%%
-CHASE_FOLDER = r"L:\Lopez Laboratory - NEURO\Xiuqi\ELA_MDMA\April_2026\chase"
-EXPS = ["female_P42","male_P35"]
 #%% Mouse level
 def pair_to_mouse(df):
-    cols = ["mouse", "video", "time_point", "box", "date", "timestamp", "start", "end", "frames_chasing", "frames_chased", "event_chasing", "event_chased"]
+    # df should have: video,pair,start,end,duration,chaser,chased,box,date,timestamp,time_point
+
+    cols = ["mouse", "video", "box", "date", "timestamp", "start_frame", "end_frame", "frames_chasing", "frames_chased", "count_chasing", "count_chased"]
     rows = []
     for _, r in df.iterrows():
         duration = r['duration']
@@ -20,39 +18,53 @@ def pair_to_mouse(df):
         rows.append({
             'mouse': r['chaser'],
             'video': r['video'],
-            'time_point': r['time_point'],
+            #'time_point': r['time_point'],
             'box': r['box'],
             'date': r['date'],
             'timestamp': r['timestamp'],
-            'start': r['start'],
-            'end': r['end'],
+            'start_frame': r['start'],
+            'end_frame': r['end'],
             'frames_chasing': duration,
             'frames_chased': 0,
-            'event_chasing': 1,
-            'event_chased': 0
+            'count_chasing': 1,
+            'count_chased': 0
         })
 
         # chased row
         rows.append({
             'mouse': r['chased'],
             'video': r['video'],
-            'time_point': r['time_point'],
+            #'time_point': r['time_point'],
             'box': r['box'],
             'date': r['date'],
             'timestamp': r['timestamp'],
-            'start': r['start'],
-            'end': r['end'],
+            'start_frame': r['start'],
+            'end_frame': r['end'],
             'frames_chasing': 0,
             'frames_chased': duration,
-            'event_chasing': 0,
-            'event_chased': 1
+            'count_chasing': 0,
+            'count_chased': 1
         })
 
     return pd.DataFrame(rows,columns=cols)
 
+#%%
+# Define paths
+main_path = "/Volumes/labs/Lopez Laboratory - NEURO/Xiuqi/ELA_MDMA"
+batches = ['April','July']
+exps = {'April':['male_P35','female_P42'],'July':['female_P35','male_P42']}
+time_points = ['baseline','MDMA']
 
-for exp in EXPS:
-    df_pair = pd.read_csv(os.path.join(CHASE_FOLDER,exp,'chase_raw_events.csv'))
-    df_mouse = pair_to_mouse(df_pair)
-    df_mouse.to_csv(os.path.join(CHASE_FOLDER,exp,'chase_events_by_mouse.csv'),index=False)
+for b in batches:
+    print(b)
+    path_chase = os.path.join(main_path,f'{b}_2026','chase')
+    for exp in exps[b]:
+        print(exp)
+        for t in time_points:
+            print(t)
+            df_pair = pd.read_csv(os.path.join(path_chase,exp,t,'chase_events.csv'))
+            df_mouse = pair_to_mouse(df_pair)
+            df_mouse.to_csv(os.path.join(path_chase,exp,t,'chase_events_by_mouse.csv'),index=False)
 
+
+# %%
--- chase_stats.py
+++ chase_stats.py
@@ -1,146 +1,84 @@
 #%%
 import os
 import pandas as pd
+import numpy as np
 import utils_stats as utils
 
 #%%
-CHASE_FOLDER = r"L:\Lopez Laboratory - NEURO\Xiuqi\ELA_MDMA\April_2026\chase"
+# customized functions
+def chase_regroup_by_timebin(df_1h,resolution,fps=25,group_base = ['day','phase','box','mouse']):
+    '''regroup and compute sums and event counts. 
+    '''
+    df = df_1h.copy()
+    df = utils.add_timebin_labels(df,resolution)
+    group_cols = group_base + ['time_bin','time_window']
 
-df_mouse = pd.read_csv(os.path.join(CHASE_FOLDER,'chase_events_by_mouse.csv'))
-# cols in events df: mouse, video, batch, time_point, stress, box, date, timestamp, start, end, frames_chasing, frames_chased, event_chasing, event_chased
 
-df_mouse = utils.compute_event_times(df=df_mouse)
-# 'recording_start', 'event_start', 'event_end'
-df_mouse = utils.add_time_labels(df=df_mouse)
-# 'CT_hour', 'phase', 'ZT_day','ZT_hour'
-fps = 25
-df_mouse['time_chasing'] = df_mouse['frames_chasing'] / fps
-df_mouse['time_chased']= df_mouse['frames_chased'] / fps
-# cols needed
-cols = ['mouse','box','batch','time_point', 'stress',
-        'CT_hour', 'phase', 'ZT_day', 'ZT_hour',
-        'time_chasing', 'time_chased', 'event_chasing', 'event_chased']
-
-df = df_mouse[cols]
-
-## 1- Group by hour
-groupby_cols = ['mouse','box','batch','time_point','stress','ZT_day','phase','ZT_hour','CT_hour']
-
-summary_df = (
-    df
-    .groupby(groupby_cols)
-    .agg(
-        total_time_chasing=('time_chasing', 'sum'),
-        total_time_chased=('time_chased', 'sum'),
-        total_event_chasing=('event_chasing', 'sum'),
-        total_event_chased=('event_chased', 'sum')
+    group_df = (
+        df.groupby(group_cols,observed=True)
+        .agg(
+            frames_chasing = ('frames_chasing','sum'),
+            frames_chased = ('frames_chased','sum'),
+            chasing_count = ('count_chasing','sum'),
+            chased_count = ('count_chased','sum')
+        )
+        .reset_index()
     )
-    .reset_index()
-)
+    group_df['chasing_duration'] = group_df['frames_chasing'] / fps
+    group_df['chased_duration'] = group_df['frames_chased'] / fps
+    group_df['chasing_mean_duration'] = np.where(group_df['chasing_count'] > 0, group_df['chasing_duration'] / group_df['chasing_count'],0)
+    group_df['chased_mean_duration'] = np.where(group_df['chased_count'] > 0, group_df['chased_duration'] / group_df['chased_count'],0)
 
-## Add conditions and normalize by nest
-cond_cols = ['mouse','box','batch','time_point','stress','phase_day','phase','timebin','total_time_outside_s','in_utero_background','mouse_id']
-df_cond = pd.read_csv(r"L:\Lopez Laboratory - NEURO\Kelli\Kelli_Nicole_in_utero_psilo_summer_2025\ROIs\all_rois_hour.csv",usecols=cond_cols)
-df_cond = df_cond.rename(columns={'phase_day':'ZT_day','timebin':'CT_hour'})
-summary_df['ZT_day'] = pd.to_datetime(summary_df['ZT_day']).dt.date
-df_cond['ZT_day'] = pd.to_datetime(df_cond['ZT_day']).dt.date
+    keep_cols = ['day','phase','box','mouse','time_bin','time_window','chasing_duration','chasing_count','chasing_mean_duration','chased_duration','chased_count','chased_mean_duration']
+    return group_df[keep_cols]
 
-summary_df = summary_df.merge(df_cond,how='outer',on= ['mouse','box','batch','time_point','stress','ZT_day','phase','CT_hour'])
+def chase_normalize_by_nest(df,nest_df,group_cols=['day','phase','box','mouse','time_bin','time_window']):
+    # duration, count, mean_duation, outside_nest_duration, duration_fraction, event_rate
+    nest_df = nest_df.rename(columns = {'total_time':'nest_duration','count':'nest_count','avg_time':'nest_mean_duration','outside_total_time':'outside_nest_duration'})
+    #nest_df = nest_df.rename(columns = {'duration':'nest_duration','count':'nest_count','mean_duration':'nest_mean_duration'})
+    nest_cols = group_cols + ['nest_duration','outside_nest_duration','nest_count','nest_mean_duration']
+    df = df.merge(nest_df[nest_cols],how='outer',on=group_cols)
+    df['chasing_duration_fraction'] = df['chasing_duration'] / df['outside_nest_duration']
+    df['chasing_event_rate'] = df['chasing_count'] / df['outside_nest_duration']
+    df['chased_duration_fraction'] = df['chased_duration'] / df['outside_nest_duration']
+    df['chased_event_rate'] = df['chased_count'] / df['outside_nest_duration']
 
-#normalization
-summary_df['chasing_norm_total_time_s'] = summary_df['total_time_chasing'] / summary_df['total_time_outside_s']
-summary_df['chasing_norm_event_count'] = summary_df['total_event_chasing'] / summary_df['total_time_outside_s']
-summary_df['chased_norm_total_time_s'] = summary_df['total_time_chased'] / summary_df['total_time_outside_s']
-summary_df['chased_norm_event_count'] = summary_df['total_event_chased'] / summary_df['total_time_outside_s']
-summary_df['chasing_avg_time_s'] = summary_df['total_time_chasing'] / summary_df['total_event_chasing']
-summary_df['chased_avg_time_s'] = summary_df['total_time_chased'] / summary_df['total_event_chased']
-summary_df['chasing_chased_ratio_time'] = summary_df['total_time_chasing'] / (summary_df['total_time_chased']+1)
-summary_df['chasing_chased_ratio_event'] = summary_df['total_event_chasing'] / (summary_df['total_event_chased']+1)
+    df['chasing_duration_ratio'] = df['chasing_duration'] / (df['chasing_duration']+df['chased_duration'])
+    df['chased_duration_ratio'] = df['chased_duration'] / (df['chasing_duration']+df['chased_duration'])
+    df['chasing_event_ratio'] = df['chasing_count'] / (df['chasing_count']+df['chased_count'])
+    df['chased_event_ratio'] = df['chased_count'] / (df['chasing_count']+df['chased_count'])
 
-summary_df.to_csv(os.path.join(CHASE_FOLDER,'chase_summary_hour.csv'),index=False)
+    return df
 
-## 2- Group by phase
-groupby_cols = ['mouse','box','batch','time_point','stress','ZT_day','phase']
+#%%
+# add labels -> regroup
+main_path = "/Volumes/labs/Lopez Laboratory - NEURO/Xiuqi/ELA_MDMA"
+batches = ['April','July']
+exps = {'April':['male_P35','female_P42'],'July':['female_P35','male_P42']}
+time_points = ['baseline','MDMA']
 
-summary_df = (
-    df
-    .groupby(groupby_cols)
-    .agg(
-        total_time_chasing=('time_chasing', 'sum'),
-        total_time_chased=('time_chased', 'sum'),
-        total_event_chasing=('event_chasing', 'sum'),
-        total_event_chased=('event_chased', 'sum')
-    )
-    .reset_index()
-)
+cols_complete = ['video','date','ZT_day','day','ZT_hour','CT_hour','phase','box','mouse','event_start','event_end','start_frame','end_frame','frames_chasing','frames_chased','count_chasing','count_chased']
+cols_1h = ['day','ZT_hour','phase','box','mouse','frames_chasing','frames_chased','count_chasing','count_chased']
+for b in batches:
+    print(b)
+    path_chase = os.path.join(main_path,f'{b}_2026','chase')
+    for exp in exps[b]:
+        print(exp)
+        for t in time_points:
+            print(t)
+            df = pd.read_csv(os.path.join(path_chase,exp,t,'chase_events_by_mouse.csv'))
+            df = utils.compute_event_times(df)
+            # skipped the 1h separation since chasing events are usually short
+            df = utils.add_time_labels(df)
+            df = utils.add_day_order(df)
+            if b=='April' and exp=='male_P35' and t=='MDMA':
+                df['day'] = df['day'] + 1
+            df[cols_complete].to_csv(os.path.join(path_chase,exp,t,'chase_1h_sanity_check.csv'),index=False)
+            df_1h = df[cols_1h]
+            for res in [1,2,3,4,6,12]:
+                df_res = chase_regroup_by_timebin(df_1h,resolution=res)
+                df_res = chase_normalize_by_nest(df_res,
+                                                 nest_df=pd.read_csv(os.path.join(main_path,f'{b}_2026','nest',exp,t,f'nest_{res}h.csv')))
+                df_res.to_csv(os.path.join(path_chase,exp,t,f'chase_{res}h.csv'),index=False)
 
-
-## Add conditions and normalize by nest
-cond_cols = ['mouse','box','batch','time_point','stress','phase_day','phase','total_time_outside_s','in_utero_background','mouse_id']
-df_cond = pd.read_csv(r"L:\Lopez Laboratory - NEURO\Kelli\Kelli_Nicole_in_utero_psilo_summer_2025\ROIs\all_rois_phase.csv",usecols=cond_cols)
-df_cond = df_cond.rename(columns={'phase_day':'ZT_day'})
-summary_df['ZT_day'] = pd.to_datetime(summary_df['ZT_day']).dt.date
-df_cond['ZT_day'] = pd.to_datetime(df_cond['ZT_day']).dt.date
-
-set_summary = set(map(tuple, summary_df[groupby_cols].values))
-set_cond = set(map(tuple, df_cond[groupby_cols].values))
-
-print("Keys in both:", len(set_summary & set_cond))
-print("Only in summary:", len(set_summary - set_cond))
-print("Only in cond:", len(set_cond - set_summary))
-
-diff = list(set_summary - set_cond)
-print(diff[:10])
-diff2 = list(set_cond - set_summary)
-print(diff2[:10])
-
-summary_df = summary_df.merge(df_cond,how='outer',on=groupby_cols)
-
-#normalization
-summary_df['chasing_norm_total_time_s'] = summary_df['total_time_chasing'] / summary_df['total_time_outside_s']
-summary_df['chasing_norm_event_count'] = summary_df['total_event_chasing'] / summary_df['total_time_outside_s']
-summary_df['chased_norm_total_time_s'] = summary_df['total_time_chased'] / summary_df['total_time_outside_s']
-summary_df['chased_norm_event_count'] = summary_df['total_event_chased'] / summary_df['total_time_outside_s']
-summary_df['chasing_avg_time_s'] = summary_df['total_time_chasing'] / summary_df['total_event_chasing']
-summary_df['chased_avg_time_s'] = summary_df['total_time_chased'] / summary_df['total_event_chased']
-summary_df['chasing_chased_ratio_time'] = summary_df['total_time_chasing'] / (summary_df['total_time_chased']+1)
-summary_df['chasing_chased_ratio_event'] = summary_df['total_event_chasing'] / (summary_df['total_event_chased']+1)
-
-summary_df.to_csv(os.path.join(CHASE_FOLDER,'chase_summary_phase.csv'),index=False)
-
-## 3- Group by day
-groupby_cols = ['mouse','box','batch','time_point','stress','ZT_day']
-
-summary_df = (
-    df
-    .groupby(groupby_cols)
-    .agg(
-        total_time_chasing=('time_chasing', 'sum'),
-        total_time_chased=('time_chased', 'sum'),
-        total_event_chasing=('event_chasing', 'sum'),
-        total_event_chased=('event_chased', 'sum')
-    )
-    .reset_index()
-)
-
-
-## Add conditions and normalize by nest
-cond_cols = ['mouse','box','batch','time_point','stress','phase_day','total_time_outside_s','in_utero_background','mouse_id']
-df_cond = pd.read_csv(r"L:\Lopez Laboratory - NEURO\Kelli\Kelli_Nicole_in_utero_psilo_summer_2025\ROIs\all_rois_day.csv",usecols=cond_cols)
-df_cond = df_cond.rename(columns={'phase_day':'ZT_day'})
-summary_df['ZT_day'] = pd.to_datetime(summary_df['ZT_day']).dt.date
-df_cond['ZT_day'] = pd.to_datetime(df_cond['ZT_day']).dt.date
-
-summary_df = summary_df.merge(df_cond,how='outer',on=groupby_cols)
-
-#normalization
-summary_df['chasing_norm_total_time_s'] = summary_df['total_time_chasing'] / summary_df['total_time_outside_s']
-summary_df['chasing_norm_event_count'] = summary_df['total_event_chasing'] / summary_df['total_time_outside_s']
-summary_df['chased_norm_total_time_s'] = summary_df['total_time_chased'] / summary_df['total_time_outside_s']
-summary_df['chased_norm_event_count'] = summary_df['total_event_chased'] / summary_df['total_time_outside_s']
-summary_df['chasing_avg_time_s'] = summary_df['total_time_chasing'] / summary_df['total_event_chasing']
-summary_df['chased_avg_time_s'] = summary_df['total_time_chased'] / summary_df['total_event_chased']
-summary_df['chasing_chased_ratio_time'] = summary_df['total_time_chasing'] / (summary_df['total_time_chased']+1)
-summary_df['chasing_chased_ratio_event'] = summary_df['total_event_chasing'] / (summary_df['total_event_chased']+1)
-
-summary_df.to_csv(os.path.join(CHASE_FOLDER,'chase_summary_day.csv'),index=False)
+# %%
